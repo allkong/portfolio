c8a3c22781f5917d33eabb69638b5f0773de5c45
Dabin Jeong <allempti@gmail.com>
2025-09-21 05:52:51 +0900

    rename: 페이지에서 section 경로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Header from "@/components/header";
 import Hero from "@/components/hero";
-import About from "@/components/about";
-import Skills from "@/components/skills";
-import Projects from "@/components/projects";
-import Contact from "@/components/contact";
+import About from "@/components/section/about";
+import Skills from "@/components/section/skills";
+import Projects from "@/components/section/projects";
+import Contact from "@/components/section/contact";
 
 const Home = () => {
   return (
