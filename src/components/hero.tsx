a4e6c764a17d9545a5fc4b43fb10336c88b60c4f
Dabin Jeong <allempti@gmail.com>
2025-09-21 05:52:34 +0900

    rename: 섹션 파일 section 폴더로 이동

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import HeroTitle from "./hero-title";
+
 const Hero = () => {
   return (
-    <section className="bg-[url('/images/paper-texture.jpg')] bg-cover w-full h-screen">
-      Hero
+    <section className="bg-[url('/images/paper-texture.jpg')] bg-cover w-full h-screen pt-14">
+      <p>다빈이코드</p>
+      <HeroTitle />
+      <Image
+        src="/images/profile.png"
+        alt="프로필"
+        width={400}
+        height={0}
+        className=""
+      />
     </section>
   );
 };
