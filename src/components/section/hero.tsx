336dcf1aa2401f90b4d3b92661f0ac8087883a75
allkong <allempti@gmail.com>
2025-09-21 17:29:42 +0900

    design: HeroTitle 너비 max 수정

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -5,7 +5,7 @@ import HeroTitle from '@images/hero-title.svg';
 const Hero = () => {
   return (
     <section className="hero-bg flex h-dvh w-full flex-col items-center justify-end bg-cover">
-      <HeroTitle className="w-2xs flex-1 sm:w-sm lg:w-lg xl:w-sm" />
+      <HeroTitle className="max-w-2xs flex-1 sm:max-w-sm lg:max-w-lg xl:max-w-sm" />
       <div className="relative flex max-h-3/5 w-full items-center justify-center">
         <Image
           src="/images/profile.png"
