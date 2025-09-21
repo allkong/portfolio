7fa111fdd7c28d0f8a3cfbf2307dafec9f6e7d3d
allkong <allempti@gmail.com>
2025-09-22 05:12:17 +0900

    feat: SUIT 폰트 전체 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Special_Elite } from 'next/font/google';
+import { Special_Elite } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const suit = localFont({
+  src: '../../public/fonts/SUIT-Variable.woff2',
+  variable: '--font-suit',
 });
 
 const specialElite = Special_Elite({
@@ -30,11 +26,7 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${specialElite.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${suit.variable} ${specialElite.variable} antialiased`}>{children}</body>
     </html>
   );
 };
