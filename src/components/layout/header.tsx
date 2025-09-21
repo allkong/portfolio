6dda120b376b381c48b2069d8a7cc900d047aae6
allkong <allempti@gmail.com>
2025-09-22 07:57:23 +0900

    design: header 배경 블러 처리

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Logo from '@images/logo.svg';
 import MenuIcon from '@icons/menu.svg';
@@ -16,37 +16,43 @@ const SECTIONS = [
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? 'hidden' : '';
+  }, [isOpen]);
+
   const handleClose = () => {
     setIsOpen(false);
   };
 
   return (
-    <header className={`font-special-elite fixed top-0 z-20 w-full`}>
-      <nav className="flex items-center justify-between px-4 py-3 sm:px-20 sm:py-5">
-        <a href="#hero">
-          <Logo />
-        </a>
+    <div className="font-special-elite">
+      <header className="fixed top-0 z-20 w-full backdrop-blur-[2px]">
+        <nav className="flex items-center justify-between px-4 py-3 sm:px-20 sm:py-5">
+          <a href="#hero">
+            <Logo />
+          </a>
 
-        {/* desktop 내비게이션 */}
-        <ul className="hidden gap-10 text-lg sm:flex">
-          {SECTIONS.map((s) => (
-            <li key={s.id}>
-              <a href={`#${s.id}`}>{s.label}</a>
-            </li>
-          ))}
-        </ul>
+          {/* desktop 내비게이션 */}
+          <ul className="hidden gap-10 text-lg sm:flex">
+            {SECTIONS.map((s) => (
+              <li key={s.id}>
+                <a href={`#${s.id}`}>{s.label}</a>
+              </li>
+            ))}
+          </ul>
 
-        {/* mobile 햄버거 버튼 */}
-        <button
-          aria-label="open menu"
-          aria-haspopup="dialog"
-          aria-expanded={isOpen}
-          onClick={() => setIsOpen(true)}
-          className="sm:hidden"
-        >
-          <MenuIcon />
-        </button>
-      </nav>
+          {/* mobile 햄버거 버튼 */}
+          <button
+            aria-label="open menu"
+            aria-haspopup="dialog"
+            aria-expanded={isOpen}
+            onClick={() => setIsOpen(true)}
+            className="sm:hidden"
+          >
+            <MenuIcon />
+          </button>
+        </nav>
+      </header>
 
       {/* mobile 메뉴 오버레이 */}
       {isOpen && (
@@ -79,13 +85,17 @@ const Header = () => {
             </ul>
 
             <div className="flex justify-center space-x-3">
-              <a>Github</a>
-              <a>Tistory</a>
+              <a href="https://github.com/allkong" target="_blank" rel="noreferrer">
+                Github
+              </a>
+              <a href="https://allkong.tistory.com" target="_blank" rel="noreferrer">
+                Tistory
+              </a>
             </div>
           </div>
         </div>
       )}
-    </header>
+    </div>
   );
 };
 
