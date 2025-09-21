6c4494db7be3a9329f8c30267b7130347e245fc5
allkong <allempti@gmail.com>
2025-09-21 18:38:59 +0900

    refactor: 프로필 image 정적 임포트

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 
 import HeroTitle from '@images/hero-title.svg';
+import profile from '@images/profile.png';
 
 const Hero = () => {
   return (
@@ -11,7 +12,7 @@ const Hero = () => {
       <HeroTitle className="group max-w-2xs flex-1 sm:max-w-sm lg:max-w-lg xl:max-w-sm 2xl:max-w-md [&_g]:[transform-origin:center] [&_g]:transform [&_g]:transition-transform [&_g]:duration-200 [&_g]:ease-out [&_g]:[transform-box:fill-box] motion-reduce:[&_g]:transition-none [&_g:hover]:-translate-y-4" />
       <div className="relative flex max-h-3/5 w-full items-center justify-center">
         <Image
-          src="/images/profile.png"
+          src={profile}
           alt="프로필"
           width={1200}
           height={0}
