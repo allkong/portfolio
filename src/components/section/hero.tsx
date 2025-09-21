dd9d965729756cc7d3ec1dfdc474ab28716c1ece
Dabin Jeong <allempti@gmail.com>
2025-09-21 15:10:35 +0900

    design: HeroTitle, 프로필 이미지 디자인 수정

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -5,14 +5,16 @@ import HeroTitle from '@images/hero-title.svg';
 const Hero = () => {
   return (
     <section className="hero-bg flex h-dvh w-full flex-col items-center justify-end bg-cover">
-      <HeroTitle className="w-2xs flex-1 sm:w-md sm:flex-none" />
-      <Image
-        src="/images/profile.png"
-        alt="프로필"
-        width={500}
-        height={0}
-        className="w-96 sm:mt-8"
-      />
+      <HeroTitle className="w-2xs flex-1 sm:w-sm lg:w-lg xl:w-sm" />
+      <div className="relative flex max-h-3/5 w-full items-center justify-center">
+        <Image
+          src="/images/profile.png"
+          alt="프로필"
+          width={1200}
+          height={0}
+          className="h-auto max-h-full object-contain"
+        />
+      </div>
     </section>
   );
 };
